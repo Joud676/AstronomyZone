81af4ed5b11e5029dee0bbfef3d0a50052b33092
Joud Jalal <joudjalal5@gmail.com>
2025-04-18 23:26:35 +0300

    Adding Codes

---
+++ JS/memory.js
@@ -0,0 +1,178 @@
+const xhr = new XMLHttpRequest();
+const key = "YLEFkgxyjrYiQziAhkkVuzGg5r93JkQXvS41DOwD";
+
+
+async function fetchAPOD(endpoint) {
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) throw new Error("Failed to fetch");
+        return await response.json();
+    } catch (error) {
+        displayError(error.message);
+    }
+}
+
+
+let apodImages = [];
+
+async function startGame() {
+
+    const startInput = document.getElementById("start-date").value;
+    const endInput = document.getElementById("end-date").value;
+
+    if (!startInput || !endInput) {
+        displayError("Kindly choose the picture's start and end date");
+        return;
+    }
+
+    const startDate = new Date(startInput);
+    const endDate = new Date(endInput);
+
+    if (endDate < startDate) {
+        displayError("End date must be after start date");
+        return;
+    }
+
+    const diffTime = Math.abs(endDate - startDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+
+    if (diffDays < 4) {
+        displayError("Please select at least 4 days");
+        return;
+    }
+
+    apodImages = [];
+    let currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+        const formattedDate = currentDate.toISOString().split("T")[0];
+        const endpoint = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${formattedDate}`;
+        const data = await fetchAPOD(endpoint);
+        if (data) apodImages.push(data);
+        apodImages.push(data);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+
+    apodImages = shuffle(apodImages);
+    memoryGameStart(apodImages);
+}
+
+function shuffle(array) {
+    const copy = [...array];
+    for (let i = copy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+    return copy;
+}
+
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
+
+function memoryGameStart(images) {
+    const gameBoard = document.getElementById("game-board");
+    const errorDiv = document.getElementById("fetchedData");
+    errorDiv.innerHTML = "";
+    gameBoard.innerHTML = "";
+
+    images.forEach((imageData, index) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.style.padding = "15px";
+        card.dataset.index = index;
+
+        card.innerHTML = `
+            <div class="card-inner">
+                <div class="card-front">
+🚀
+                </div>
+                <div class="card-back">
+                    <img src="${imageData.url}" alt="${imageData.title}" />  <!-- The image is on the back -->
+                </div>
+            </div>
+        `;
+
+        card.addEventListener("click", flipCard);
+        gameBoard.appendChild(card);
+    });
+}
+
+
+function flipCard() {
+    if (lockBoard) return;
+    const clickedCard = this;
+
+    if (clickedCard === firstCard) return;
+
+    clickedCard.classList.add("flipped");
+
+    if (!firstCard) {
+        firstCard = clickedCard;
+        return;
+    }
+
+    secondCard = clickedCard;
+    lockBoard = true;
+
+    const firstIndex = firstCard.dataset.index;
+    const secondIndex = secondCard.dataset.index;
+
+    const firstImage = apodImages[firstIndex];
+    const secondImage = apodImages[secondIndex];
+
+    if (firstImage.url === secondImage.url) {
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        resetBoard();
+        checkWin();
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove("flipped");
+            secondCard.classList.remove("flipped");
+            resetBoard();
+        }, 1000);
+    }
+}
+
+
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+
+function checkWin() {
+    const allMatched = document.querySelectorAll(".card.matched").length === apodImages.length;
+    if (allMatched) {
+        const winContainer = document.getElementById("win-message");
+        winContainer.innerHTML = "";
+        const winMessage = document.createElement("p");
+        winMessage.classList.add("winMessage");
+        winMessage.textContent = "You win! 🎉";
+        winMessage.style.background = "rgba(58, 4, 87, 0.575);";
+        winMessage.style.boxShadow = "0 0 20px rgba(58, 17, 85, 0.51)";
+        winContainer.appendChild(winMessage);
+        const newButton = document.getElementById("new");
+        newButton.style.display = "inline"
+    }
+}
+
+function reset() {
+    const newButton = document.getElementById("new");
+    newButton.style.display = "none"
+    const container = document.getElementById("container");
+    container.innerHTML = "";
+}
+
+function displayError(errorMessage) {
+    const fetchedDataElement = document.getElementById("fetchedData");
+    fetchedDataElement.innerHTML = "";
+
+    const errorElement = document.createElement("p");
+    errorElement.classList.add("error");
+    errorElement.textContent = errorMessage;
+
+    fetchedDataElement.appendChild(errorElement);
+    fetchedDataElement.style.visibility = "visible";
+}
---
+++ JS/puzzle.js
@@ -0,0 +1,159 @@
+let flag = false;
+const xhr = new XMLHttpRequest();
+const key = "YLEFkgxyjrYiQziAhkkVuzGg5r93JkQXvS41DOwD";
+
+function getData() {
+    let date = document.getElementById("image-date").value;
+
+    if (date !== "" || flag) {
+        let endpoint = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`;
+        fetch(endpoint);
+    } else {
+        displayError("Kindly choose the picture date or press on Today's Picture");
+    }
+    flag = false;
+}
+
+function fetch(endpoint) {
+    xhr.open("GET", endpoint, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            displayData(response);
+        } else {
+            displayError("Failed to Fetch The Picture Of The Day. The date you selected might be different from NASA's time zone.");
+        }
+    };
+    xhr.onerror = function () {
+        displayError("An error occurred while fetching the picture");
+    };
+    xhr.send();
+}
+
+function todaysDate() {
+    let endpoint = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+    flag = true;
+    fetch(endpoint);
+}
+
+function displayData(data) {
+    const puzzleElement = document.getElementById("fetchedData");
+    puzzleElement.innerHTML = "";
+
+    // Create container for image and button
+    const imageDisplay = document.createElement("div");
+    imageDisplay.id = "image-display";
+    puzzleElement.appendChild(imageDisplay);
+
+    // Create Picture <div> and append it to the image display
+    const imgBackgroundElement = document.createElement("div");
+    imgBackgroundElement.id = "POD";
+    imageDisplay.appendChild(imgBackgroundElement);
+
+    const url = data.url;
+    const title = data.title;
+    appendPicture(imgBackgroundElement, url, title, puzzleElement);
+
+    // Create Start Puzzle button
+    const startButton = document.createElement("button");
+    startButton.id = "start-puzzle";
+    startButton.className = "button puzzle-button";
+    startButton.textContent = "Start Puzzle";
+    startButton.onclick = () => startPuzzle(url);
+    imageDisplay.appendChild(startButton);
+
+    // Create a div for Picture's Info
+    const infoElement = document.createElement("div");
+    infoElement.classList.add("infoContainer");
+    puzzleElement.appendChild(infoElement);
+
+    appendTitle(title, infoElement);
+
+    const date = data.date;
+    appendDate(infoElement, date);
+
+    const explanation = data.explanation;
+    appendExplanation(infoElement, explanation);
+
+    const copyright = data.copyright;
+    appendCopyright(infoElement, copyright);
+
+
+    const iframeContainer = document.createElement("div");
+    iframeContainer.id = "puzzle-container";
+    puzzleElement.appendChild(iframeContainer);
+
+    puzzleElement.style.visibility = "visible";
+}
+
+function startPuzzle(imageUrl) {
+    const encodedUrl = encodeURIComponent(imageUrl);
+    const iframeContainer = document.getElementById("puzzle-container");
+    iframeContainer.innerHTML = "";
+
+    const iframe = document.createElement("iframe");
+    iframe.style.width = "100%";
+    iframe.style.height = "600px";
+    iframe.style.maxHeight = "90vh";
+    iframe.style.borderStyle = "solid";
+    iframe.style.borderWidth = "2px";
+    iframe.style.borderColor = "#888";
+    iframe.allowFullScreen = true;
+    iframe.src = `https://www.jigsawexplorer.com/online-jigsaw-puzzle-player.html?frm=1&url=${encodedUrl}`;
+    iframe.title = "Jigsaw Puzzle";
+
+    iframeContainer.appendChild(iframe);
+
+    iframeContainer.style.display = "block";
+
+    iframeContainer.scrollIntoView({ behavior: 'smooth' });
+}
+
+function appendPicture(imgBackgroundElement, url, title, puzzleElement) {
+    const pictureElement = document.createElement("img");
+    pictureElement.id = "image";
+    pictureElement.src = url;
+    pictureElement.alt = title;
+    imgBackgroundElement.appendChild(pictureElement);
+    puzzleElement.style.visibility = "visible";
+}
+
+function appendTitle(title, infoElement) {
+    const titleElement = document.createElement("p");
+    titleElement.classList.add("ImgInfo");
+    titleElement.textContent = `Title: ${title}`;
+    infoElement.appendChild(titleElement);
+}
+
+function appendDate(infoElement, date) {
+    const dateElement = document.createElement("p");
+    dateElement.classList.add("ImgInfo");
+    dateElement.textContent = `Date: ${date}`;
+    infoElement.appendChild(dateElement);
+}
+
+function appendCopyright(infoElement, copyright) {
+    const copyrightElement = document.createElement("p");
+    copyrightElement.classList.add("ImgInfo");
+    copyrightElement.textContent = `Copyright: ${copyright}`;
+    infoElement.appendChild(copyrightElement);
+}
+
+function appendExplanation(infoElement, explanation) {
+    const explanationElement = document.createElement("p");
+    explanationElement.classList.add("ImgInfo");
+    explanationElement.textContent = `Explanation: ${explanation}`;
+    infoElement.appendChild(explanationElement);
+}
+
+function displayError(errorMessage) {
+    const puzzleElement = document.getElementById("fetchedData");
+    puzzleElement.innerHTML = "";
+
+    const errorElement = document.createElement("p");
+    errorElement.classList.add("error");
+    errorElement.textContent = errorMessage;
+
+    puzzleElement.appendChild(errorElement);
+    puzzleElement.style.visibility = "visible";
+}
